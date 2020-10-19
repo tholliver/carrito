e6d2078813c564056aec254dd03c50af876c04d8
miguel <miguelvargas@zoho.com>
2020-10-19 17:10:36 -0400

    Eveything setup to run. Ready to add team

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nombres from "./Nombres";
 
 function App() {
+  /* HERE SHOULD */
+  const team = [
+    {
+      nombre: "Miguel LaMadness",
+      gitUser: "miguel" /*Nombre de usario de git Local*/,
+    },
+    {
+      nombre: "Tu nombre",
+      gitUser: "userGitName" /*Nombre de usario de git Local*/,
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nombres team={team}/>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
