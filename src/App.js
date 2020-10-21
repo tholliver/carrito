6e61ea8139d1b99671cd9b11b4a767e80bb506e8
garySZA <garylvlsoliz@gmail.com>
2020-10-20 20:20:00 -0400

    Interfaz Carrito de Compras

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Nombres from "./Nombres";
+import "./style.css";
 
 function App() {
   /* HERE SHOULD */
@@ -21,11 +22,6 @@ function App() {
     },
 
 /*aQUI */
-
-
-
-
-
     {
       nombre: "Tu nombre",
       gitUser: "userGitName" /*Nombre de usario de git Local*/,
@@ -51,4 +47,68 @@ function App() {
   );
 }
 
-export default App;
+function tabla() {
+  return(
+    <div id = "styleTable">
+      <table id="tablaModelo">
+        <tr>
+          <th>Producto</th>
+          <th>Precio Unitario</th>
+          <th>Descuento Unitario</th>
+          <th>Cantidad</th>
+          <th>Total Producto</th>
+        </tr>
+        <tr>
+          <td>Producto1</td>
+          <td>Precio1</td>
+          <td>Descuento Unitario1</td>
+          <td>cantidad1</td>
+          <td>Total Producto1</td>
+        </tr>
+        <tr>
+          <td>Producto2</td>
+          <td>Precio2</td>
+          <td>Descuento Unitario2</td>
+          <td>cantidad2</td>
+          <td>Total Producto2</td>
+        </tr>
+        <tr>
+          <td>Producto3</td>
+          <td>Precio3</td>
+          <td>Descuento Unitario3</td>
+          <td>cantidad3</td>
+          <td>Total Producto3</td>
+        </tr>
+        <tr>
+          <td>Producto4</td>
+          <td>Precio4</td>
+          <td>Descuento Unitario4</td>
+          <td>cantidad4</td>
+          <td>Total Producto4</td>
+        </tr>
+        <tr>
+          <td>Producto5</td>
+          <td>Precio5</td>
+          <td>Descuento Unitario5</td>
+          <td>cantidad5</td>
+          <td>Total Producto5</td>
+        </tr>
+        <tr id="contenidoTabla"></tr>
+      </table>
+    </div>
+  );
+}
+
+function botones() {
+  return(
+    <div>
+      <button>ACEPTAR</button>
+      <button>CANCELAR</button>
+    </div>
+  );
+}
+
+
+
+
+export default tabla;
