eabe6ef446315e384acf4b31190ed4338084cd39
garySZA <garylvlsoliz@gmail.com>
2020-10-30 17:00:15 -0400

    Mejorando estructura de elementos

--- src/pages/cart/CartItem.js
+++ src/pages/cart/CartItem.js
@@ -26,10 +26,11 @@ const CartItem = ({ product }) => {
         <h5 className="mb-1">{product.name}</h5>
         <p className="mb-1">Precio: {formatNumber(product.price)} </p>
       </div>
-      <div className="col-sm-2 p-2 text-center ">
+      <div className="col-sm-2 p-2 text-center container-cantidad">
         <p className="mb-0">Cantidad: {product.quantity}</p>
       </div>
-      <div className="col-sm-4 p-2 text-right">
+      <div className="col-sm-4 p-2 text-right contenedor-botones-modificacion">
+        <div className="container-botons">
         <button
           onClick={() => increase(product)}
           className="btn btn-primary btn-sm mr-2 mb-1"
@@ -61,7 +62,8 @@ const CartItem = ({ product }) => {
           >
             <TrashIcon width={"20px"} />
           </button>
-        }
+          }
+        </div>
       </div>
     </div>
   );
