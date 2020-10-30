af17d6fecd094b51a2361b1a076678946314418b
garySZA <garylvlsoliz@gmail.com>
2020-10-30 13:30:59 -0400

    .

--- src/pages/cart/CartProducts.js
+++ src/pages/cart/CartProducts.js
@@ -13,6 +13,7 @@ const CartProducts = () => {
 
                 {
                     cartItems.map(product =>  <CartItem key={product.id} product={product}/>)
+                    /*probando*/
                 }
 
             </div>
