8c9566bd69c6fe5fa3443ecdadbcdb84a6737526
miguel <miguelvargas@zoho.com>
2020-11-18 19:07:50 -0400

    treble s

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -10,56 +10,7 @@ import "./Styles.css";
 
 const Payment = () => {
   
-  function func1() {
-    let current_datetime = new Date();
-    let formatted_date =
-      current_datetime.getFullYear() +
-      "-" +
-      (current_datetime.getMonth() + 1) +
-      "-" +
-      current_datetime.getDate() +
-      " " +
-      current_datetime.getHours() +
-      ":" +
-      current_datetime.getMinutes() +
-      ":" +
-      current_datetime.getSeconds();
-    
-    const newPedido = [
-      {
-        direccion: "sameHouseEveryone",
-        fechaPedido: formatted_date,
-        cantidadTotal: itemCount,
-        totalPagar: total,
-        idclienteP: 1,
-      },
-    ];
-
-    axios
-      .post("https://alfasoft-api.herokuapp.com/pedido", newPedido, {
-        headers: { "Content-Type": "application/json" },
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  //We are sending the last idPedido the we got.
-  function func2() {}
-
-  const arr = [];
-  cartItems.map((item) => {
-    var newADE = 7; //Maybe get (select max(idpedido) from pedido; then asign the id to newADE)
-    arr.push({ idpedido: newADE, idproducto: item.id });
-    /*if (temp.name === oldName) {
-              temp.name = name;
-          }*/
-  });
-  //Getting the tuple [{pedido_idpedido}{producto_idproducto}]
-  console.log(arr);
-  console.log(itemCount);
+  
 
   return (
     <Layout title="Pago" description="Payment page">
