57e7992a50cfa4367bf308d70c3984aec00e44e3
Neo <enrique.vallejos.777@gmail.com>
2020-11-20 17:19:28 -0400

    ventana de resumen de pedidos del cliente

--- src/pages/userOrders/MisPedidos.js
+++ src/pages/userOrders/MisPedidos.js
@@ -1,28 +1,39 @@
 import React from "react";
 import axios from "axios";
-
+import Pedidito from "./pedidito";
+import Produc from './Produc';
+var idclientePru=1;
+var num =0;
 export default class MisPedidos extends React.Component {
   state = {
     products: [],
   };
 
+ 
   componentDidMount() {
-    axios.get(`https://alfasoft-api.herokuapp.com/pedido`).then((res) => {
+    axios.get(`https://alfasoft-api.herokuapp.com/pedidousuario`).then((res) => {
       const products = res.data;
       this.setState({ products });
-      console.log(products);
+      console.log(products)
     });
   }
-
   render() {
     return (
-      <ul>
+      <div >
         {this.state.products.map((item) => (
-          <li>
-            Codigo del pedido: {item.idpedido} <br/> {item.direccion}
-          </li>
-        ))}
-      </ul>
+          <div>
+             {item.idcliente===idclientePru&&(
+               <div>
+                 Pedido número: {num=num+1}
+              <div  className="row no-gutters py-2 container contenedor-individual-producto">     
+                <Pedidito key={item.id} productito={item} product={'producto mal parido '}/>
+              </div>  
+              </div>
+            ) 
+            }         
+          </div>
+        ))}          
+      </div>
     );
   }
 }
