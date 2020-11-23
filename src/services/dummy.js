da4aa3b82ee4263b5a84f234af24b3d06ec089c5
Omar Trujillo <trujillo0643@gmail.com>
2020-11-23 11:54:26 -0400

    r

--- src/services/dummy.js
+++ src/services/dummy.js
@@ -1,3 +1,29 @@
+/*let dummyProduct = [];
+let url = "https://alfasoft-api.herokuapp.com/productos";
+
+async function getapi(url) {
+  
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var products = await response.json();
+  //console.log(products);
+  
+    for (let i = 0; i < products.length; i++) {
+      dummyProduct.push({
+        'id': products[i].idproducto,
+        'name': products[i].nombre,
+        'price': products[i].precio,
+        'photo': products[i].img,
+        'cantDisponible': products[i].cantidad,
+      });
+  
+  }
+}
+
+getapi(url);
+console.log(dummyProduct, 'dummyProduct'); */
 
 
 
