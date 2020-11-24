6a8d264a6639764fe1811fafa6580bc504d0ed9d
Omar Trujillo <trujillo0643@gmail.com>
2020-11-23 23:22:13 -0400

    nn

--- src/pages/store/ProductsGrid.js
+++ src/pages/store/ProductsGrid.js
@@ -45,7 +45,7 @@ render(){
           <div className="py-3-numero-de-productos"> <h3>{this.state.productos.length} Items</h3></div>
         </div>
       </div>
-      <div>
+      <div className='container-product'>
         {this.state.productos.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
