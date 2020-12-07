3ef5010479b0dfda30ed9f1fc88c0f38821b1311
MauraA <mauren.elvi@gmail.com>
2020-12-06 21:16:11 -0700

    cambios en el diseño de Tienda

--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -13,9 +13,6 @@ const Store = () => {
                     <h1 className="tienda">
                         Tienda<i className="fas fa-store"></i>
                     </h1>
-                    <h2 className="subtitulo">
-                        Esta es la pagina de la tienda
-                    </h2>
                 </div>
                 <ProductsGrid/>
             </div>
