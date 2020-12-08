8febc1d1cebc64d5d73136e08da0af80a4768825
Omar Trujillo <trujillo0643@gmail.com>
2020-12-08 19:51:51 -0400

    control de admin

--- src/pages/userOrders/MisPedidos.js
+++ src/pages/userOrders/MisPedidos.js
@@ -2,20 +2,28 @@ import React from "react";
 import axios from "axios";
 import Pedidito from "./pedidito";
 import "./detalles.css"
+import Cookies from "universal-cookie";
 //import Produc from './Produc';
 var idclientePru=1;
 var num =0;
+const cookies = new Cookies();
 export default class MisPedidos extends React.Component {
   state = {
+    
+    value:"Pendiente",
     products: [],
   };
-
+  onChange(e) {
+    console.log(e.target.value)
+    this.setState({value: e.target.value});
+    
+  }
  
   componentDidMount() {
     axios.get(`https://alfasoft-api.herokuapp.com/pedidousuario`).then((res) => {
       const products = res.data;
       this.setState({ products });
-      console.log(products)
+      //console.log(products)
       //estoy probando una modificacion
     });
   }
@@ -29,6 +37,13 @@ export default class MisPedidos extends React.Component {
                  <div className="cajas">
                  <div className= "nu"> 
                  Pedido número: {num=num+1}
+                  {cookies.get('tipoUsuario')==='admin' &&
+                    <select value={this.state.value} onChange={this.onChange}>
+                      <option value="Pendiente">Pendiente</option>
+                      <option value="En Camino">En Camino</option>
+                      <option value="Entregado">Entregado</option>
+                    </select>
+                  }
                  </div>
               <div  className="row-mi no-gutters py-2 container contenedor-indi">     
                 <Pedidito key={item.id} productito={item} product={'producto mal parido '}/>
