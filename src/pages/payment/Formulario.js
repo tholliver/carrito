054ae1fd9479881b4dc41a411fb9edb7921f2548
gusgm33 <gustavogonzalesmosquera@gmail.com>
2020-12-16 22:06:25 -0400

    correccion de diseño de formulario de tarjeta

--- src/pages/payment/Formulario.js
+++ src/pages/payment/Formulario.js
@@ -142,7 +142,7 @@ return (
        {/*input para ingreso del numero de tarjeta*/}
         <div className="grupo">
             <div className="subtitulos-formulario"> 
-              <p for="inputNumero"> Número de tarjeta </p> 
+              <p for="inputNumero"> Número de tarjeta: </p> 
             </div>
             <input className= "pro" type="text" id="inputNumero" minlength="13" maxlength="18" autocomplete="off"  required pattern="[0-9]+"  />
         </div>
@@ -150,17 +150,15 @@ return (
         {/*input para ingreso del nombre titular de la tarjeta*/}
         <div className="grupo">
             <div className="subtitulos-formulario">
-              <p for="inputNombre">Nombre titular de la tarjeta</p>
+              <p for="inputNombre">Nombre titular de la tarjeta:</p>
             </div>
             <input className= "pro" type="text" id="inputNombre" maxlength="30" autocomplete="off" required pattern="[a-zA-Z ]{2,254}" oninvalid="this.setCustomValidity('Hola')" />
         </div>
         
-        <br/>
         <div className="grupo expira">
           <div className="grupo-select1">
-            <p for="selectMes">Fecha de expiración </p>
+            <p for="selectMes">Fecha de expiración: </p>
           </div> 
-          
           <div className="flexbox-datos">
                   <p className= "pro2" for="inputMes">MM </p>
                   <h6 className= "pro44"> / </h6> 
@@ -178,7 +176,7 @@ return (
         
         <div className="grupo ccv">
           {/*input para ingreso del codigo de seguridad*/}
-            <p for="inputCVV">Código de seguridad</p>
+            <p for="inputCVV">Código de seguridad:</p>
             <div align="center">
             
             <input className="segu" type="text" id="inputCVV" maxlength="4"  minlength="3"  required  pattern="[0-9]+" />
@@ -189,9 +187,9 @@ return (
         </div>
         <br/>
         <div className="botones-formulario de tarjeta">
-          <button className="contenedor-btn" to="/mis-pedidos"  formnovalidate type="submit"onClick={validar}  >Realizar pago</button>
+          <button className="contenedor-btn-tarjetar" to="/mis-pedidos"  formnovalidate type="submit"onClick={validar}  >Realizar pago</button>
           &nbsp;&nbsp;&nbsp;
-          <button className="contenedor-btn" type="submit"onClick={pagoCancelado}  >Cancelar</button>
+          <button className="contenedor-btn-tarjetac" type="submit"onClick={pagoCancelado}  >Cancelar</button>
           </div>
         </form>
     </div>
