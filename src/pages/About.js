fbfd74ec7b1ed886e9dfdc3860ef7003c690ebb2
tholliver <michaelmmv@outlook.com>
2020-12-18 23:54:03 -0400

    some progress on team profiles

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
-
-  
-/*IMPORTANDO ESTILOS */
+import Profile from './Profile/index'
 
 const About = () => {
+   
+
+    const info={nombre:"Mike",bio:"lala",avatar_url:"land"};
     
- 
+    
     return ( 
         <Layout title="Acerca de" description="About page" >
             <div className="text-center mt-5">
                 <h1>Acerca de</h1>
                 <p>Documentacion consultada:<strong></strong>por AlfaSoft.</p>
+            {<Profile 
+          name={info.nombre} bio={info.bio}
+          avatar={info.avatar_url} />}
             </div>
+
+            
         </Layout>
     );
-}
+   
+  }
+
+
 export default About;
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './profile.css'
+
+const propTypes = {
+  name: PropTypes.string,
+  bio: PropTypes.string,
+  avatar: PropTypes.string,
+}
+
+const Profile = ({name, bio, avatar}) => {
+
+  const component = (
+    <div className='profile-wrapper'>
+      <figure className='figure'> 
+        <img className='avatar rounded-circle p-4' src={avatar} alt="profile"/>
+        <figcaption align='right'>
+          {name && <strong>{name}</strong>}
+            <br/>
+            {bio &&
+              <textarea readOnly='readonly' value={bio} className='responsive-textarea no-border no-select'></textarea>}
+          </figcaption>
+      </figure>
+    </div>
+  );
+  return component;
+}
+
+Profile.propTypes = propTypes;
+
+export default Profile;
