450326bc7a904220981787e8e16e3a62a23e7e99
tholliver <michaelmmv@outlook.com>
2020-12-19 00:26:04 -0400

    Fixed static text on UserBio

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import Profile from "./Profile/index";
 
+
 const About = () => {
-  const info = [{ nombre: "Mike", bio: "lala", avatar_url: "land" },{ nombre: "Mike", bio: "lala", avatar_url: "land" }];
+  const info = [{ nombre: "Mike", bio: "Some additons", avatar_url: "lets add up more text here to see if its get." },
+  { nombre: "Mike", bio: "ADDS ON", avatar_url: "lets add" }];
 
   return (
     <Layout title="Acerca de" description="About page">
       <div className="text-center mt-5">
-        <h1>Acerca de</h1>
+        <h1>Acerca del AlfaSoft</h1>
         
         <div  >
           
-          {info.map(item => <Profile name={item.nombre} bio={item.bio} avatar={item.avatar_url} />)}
+          {info.map((item) => <Profile name={item.nombre} bio={item.bio} avatar={item.avatar_url} />)}
         </div>
       </div>
     </Layout>
