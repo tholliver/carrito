0c41e2e00caa2f439acbff369c261de5a7b41089
tholliver <michaelmmv@outlook.com>
2020-12-19 00:08:51 -0400

    Fixed not view labels on TextArea

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import Layout from '../components/Layout';
-import Profile from './Profile/index'
+import React, { Component } from "react";
+import Layout from "../components/Layout";
+import Profile from "./Profile/index";
 
 const About = () => {
-   
+  const info = { nombre: "Mike", bio: "lala", avatar_url: "land" };
 
-    const info={nombre:"Mike",bio:"lala",avatar_url:"land"};
-    
-    
-    return ( 
-        <Layout title="Acerca de" description="About page" >
-            <div className="text-center mt-5">
-                <h1>Acerca de</h1>
-                <p>Documentacion consultada:<strong></strong>por AlfaSoft.</p>
-            {<Profile 
-          name={info.nombre} bio={info.bio}
-          avatar={info.avatar_url} />}
-            </div>
-
-            
-        </Layout>
-    );
-   
-  }
-
+  return (
+    <Layout title="Acerca de" description="About page">
+      <div className="text-center mt-5">
+        <h1>Acerca de</h1>
+        
+        <div  >
+          <Profile name={info.nombre} bio={info.bio} avatar={info.avatar_url} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default About;
