19484df13e1a5907244d472370886dc209cc8d02
tholliver <michaelmmv@outlook.com>
2020-12-19 01:05:24 -0400

    Adding responsive boxes

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,19 +2,31 @@ import React from "react";
 import Layout from "../components/Layout";
 import Profile from "./Profile/index";
 
-
 const About = () => {
-  const info = [{ nombre: "Mike", bio: "Some additons", avatar_url: "lets add up more text here to see if its get." },
-  { nombre: "Mike", bio: "ADDS ON", avatar_url: "lets add" }];
+  const info = [
+    { nombre: "Omar [la master]", bio: "Some additons", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Gustavo", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Luis", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Deniesse", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Maura", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Antonio", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "Gary", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+    { nombre: "La massa Bro", bio: "ADDS ON", avatar_url: "/teamPics/mike.jpg" },
+  ];
 
   return (
     <Layout title="Acerca de" description="About page">
       <div className="text-center mt-5">
-        <h1>Acerca del AlfaSoft</h1>
-        
-        <div  >
-          
-          {info.map((item) => <Profile name={item.nombre} bio={item.bio} avatar={item.avatar_url} />)}
+        <h1>Acerca de AlfaSoft</h1>
+
+        <div>
+          {info.map((item) => (
+            <Profile
+              name={item.nombre}
+              bio={item.bio}
+              avatar={item.avatar_url}
+            />
+          ))}
         </div>
       </div>
     </Layout>
